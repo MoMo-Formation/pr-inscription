cf4a7a61c45b6dc274dc320677ae76474ef31d41
Moiseagro <163756886+Moiseagro@users.noreply.github.com>
2024-03-19 09:35:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,15 @@ document.getElementById('preinscription-form').addEventListener('submit', functi
   let whatsappLink = `https://wa.me/22601368949?text=${encodedMessage}`;
   window.open(whatsappLink, '_blank');
 
-  // Affichage de la notification
-  let notification = document.getElementById('notification');
+  // Affichage de la notification spécifique au moyen de paiement
+  let paymentNotification = document.getElementById('payment-notification');
   if (payment === 'orange_money' || payment === 'wave') {
-    notification.innerHTML = '<p class="notification"><strong><em>Le paiement se fait sur le +22657547436</em></strong></p>';
+    paymentNotification.innerHTML = '<strong><em>Le paiement se fait sur le +22657547436</em></strong>';
   } else if (payment === 'nita') {
-    notification.innerHTML = '<p class="notification"><strong><em>Le paiement se fait au Burkina Faso, dans la ville de Bobo-Dioulasso, au nom de Compaoré Moïse sur le +22657547436</em></strong></p>';
+    paymentNotification.innerHTML = '<strong><em>Le paiement se fait au Burkina Faso, dans la ville de Bobo-Dioulasso, au nom de Compaoré Moïse sur le +22657547436</em></strong>';
   } else if (payment === 'moov_money') {
-    notification.innerHTML = '<p class="notification"><strong><em>Le paiement se fait sur le +22601368949</em></strong></p>';
+    paymentNotification.innerHTML = '<strong><em>Le paiement se fait sur le +22601368949</em></strong>';
   } else if (payment === 'express_point') {
-    notification.innerHTML = '<p class="notification"><strong><em>Le paiement se fait au nom de Compaoré Moïse (veuillez m\'envoyer le code de retrait sur WhatsApp)</em></strong></p>';
+    paymentNotification.innerHTML = '<strong><em>Le paiement se fait au nom de Compaoré Moïse (veuillez m\'envoyer le code de retrait sur WhatsApp)</em></strong>';
   }
-
-  // Ajout de la notification sur la capture d'écran
-  let notificationScreenshot = '<p class="notification"><strong><em>Une capture d\'écran de la confirmation du paiement est nécessaire pour valider l\'inscription.</em></strong></p>';
-  notification.insertAdjacentHTML('beforeend', notificationScreenshot);
 });
