16e062e5eee2ffa76e8eefb79b67d8ebf85b63cc
Moiseagro <163756886+Moiseagro@users.noreply.github.com>
2024-03-19 10:10:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,40 @@ document.getElementById('preinscription-form').addEventListener('submit', functi
   let session = document.getElementById('session').value;
   let price = document.querySelector('input[name="price"]:checked').value;
   let payment = document.getElementById('payment').value;
+  let paymentAddress = document.getElementById('payment_address').value;
   let paymentNumber = document.getElementById('payment_number').value;
-  let message = `Préinscription à la session : ${session}\nChoix du prix : ${price}\nMoyen de paiement : ${payment}\nNuméro de téléphone pour le paiement : ${paymentNumber}`;
+  let message = `Préinscription à la session : ${session}\nChoix du prix : ${price}\nMoyen de paiement : ${payment}\nAdresse du moyen de paiement : ${paymentAddress}\nNuméro de téléphone pour le paiement : ${paymentNumber}`;
 
   let encodedMessage = encodeURIComponent(message);
   let whatsappLink = `https://wa.me/22601368949?text=${encodedMessage}`;
   window.open(whatsappLink, '_blank');
 });
 
-// Affichage des informations de paiement en fonction du choix du moyen de paiement
+// Affichage de l'adresse du moyen de paiement en fonction du choix du moyen de paiement
 document.getElementById('payment').addEventListener('change', function() {
-  let paymentInfo = document.getElementById('payment-info');
+  let paymentAddress = document.getElementById('payment_address');
   let payment = document.getElementById('payment').value;
-  let paymentText = '';
+  let addressText = '';
 
   switch (payment) {
     case 'orange_money':
+      addressText = '+22657547436 (Wend Lassida Ismael Moise Compaore)';
+      break;
     case 'wave':
-      paymentText = 'Les paiements se font sur :\nOrange Money : +22657547436\nWave : +22657547436';
+      addressText = '+22657547436';
       break;
     case 'moov_money':
-      paymentText = 'Moov Money : +22601368949';
+      addressText = '+22601368949';
       break;
     case 'nita':
-      paymentText = 'Nita : Burkina Faso, Bobo-Dioulasso à Moïse Compaoré au +22657547436';
+      addressText = 'Burkina Faso, Bobo-Dioulasso à Moïse Compaoré au +22657547436';
       break;
     case 'express_point':
-      paymentText = 'Point Express (Ecobank) : +22657547436';
+      addressText = 'Compaoré Moïse, +22657547436';
       break;
     default:
-      paymentText = '';
+      addressText = '';
   }
 
-  paymentInfo.textContent = paymentText;
+  paymentAddress.value = addressText;
 });
