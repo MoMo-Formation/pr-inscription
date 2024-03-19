2b6230f9e132d061a58e6e85a2b2a30b2e23ee2a
Moiseagro <163756886+Moiseagro@users.noreply.github.com>
2024-03-19 09:56:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,31 @@ document.getElementById('preinscription-form').addEventListener('submit', functi
   let encodedMessage = encodeURIComponent(message);
   let whatsappLink = `https://wa.me/22601368949?text=${encodedMessage}`;
   window.open(whatsappLink, '_blank');
+});
 
-  // Affichage de la notification spécifique au moyen de paiement
-  let paymentNotification = document.getElementById('payment-notification');
-  if (payment === 'orange_money' || payment === 'wave') {
-    if (paymentNotification) {
-      paymentNotification.innerHTML = '<strong><em>Le paiement se fait sur le +22657547436</em></strong>';
-    }
-  } else if (payment === 'nita') {
-    if (paymentNotification) {
-      paymentNotification.innerHTML = '<strong><em>Le paiement se fait au Burkina Faso, dans la ville de Bobo-Dioulasso, au nom de Compaoré Moïse sur le +22657547436</em></strong>';
-    }
-  } else if (payment === 'moov_money') {
-    if (paymentNotification) {
-      paymentNotification.innerHTML = '<strong><em>Le paiement se fait sur le +22601368949</em></strong>';
-    }
-  } else if (payment === 'express_point') {
-    if (paymentNotification) {
-      paymentNotification.innerHTML = '<strong><em>Le paiement se fait au nom de Compaoré Moïse (veuillez m\'envoyer le code de retrait sur WhatsApp)</em></strong>';
-    }
+// Affichage des informations de paiement en fonction du choix du moyen de paiement
+document.getElementById('payment').addEventListener('change', function() {
+  let paymentInfo = document.getElementById('payment-info');
+  let payment = document.getElementById('payment').value;
+  let paymentText = '';
+
+  switch (payment) {
+    case 'orange_money':
+    case 'wave':
+      paymentText = 'Les paiements se font sur :\nOrange Money : +22657547436\nWave : +22657547436';
+      break;
+    case 'moov_money':
+      paymentText = 'Moov Money : +22601368949';
+      break;
+    case 'nita':
+      paymentText = 'Nita : Burkina Faso, Bobo-Dioulasso à Moïse Compaoré au +22657547436';
+      break;
+    case 'express_point':
+      paymentText = 'Point Express (Ecobank) : +22657547436';
+      break;
+    default:
+      paymentText = '';
   }
+
+  paymentInfo.textContent = paymentText;
 });
