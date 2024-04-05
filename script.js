3e63b31ad17498d18b0e76d5bdf10695c0e8d178
Moiseagro <163756886+Moiseagro@users.noreply.github.com>
2024-04-05 10:47:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-// Ajouter un gestionnaire d'événements au clic sur le bouton d'envoi
-document.getElementById('submit-btn').addEventListener('click', function() {
-  sendWhatsAppMessage();
+document.getElementById('preinscription-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  let session = document.getElementById('session').value;
+  let price = document.querySelector('input[name="price"]:checked').value;
+  let payment = document.getElementById('payment').value;
+
+  // Construction du message avec encadrement par des "*"
+  let message = `*Préinscription à la session :*\n${session}\n\n*Prix :*\n${price} FCFA\n\n*Moyen de paiement :*\n${payment}`;
+
+  let encodedMessage = encodeURIComponent(message);
+  let whatsappLink = `https://wa.me/22601368949?text=${encodedMessage}`;
+  window.open(whatsappLink, '_blank');
 });
-
-// Fonction pour envoyer le message WhatsApp
-function sendWhatsAppMessage() {
-  // Récupérer les valeurs de tous les champs du formulaire
-  var session = document.getElementById('session').value;
-  var price = document.querySelector('input[name="price"]:checked').value;
-  var payment = document.getElementById('payment').value;
-  var paymentAddress = document.getElementById('payment_address').value;
-  var nom = document.getElementById('nom').value;
-  var prenom = document.getElementById('prenom').value;
-  var whatsapp = document.getElementById('whatsapp').value;
-
-  // Construire le message WhatsApp en concaténant toutes les réponses
-  var whatsappMessage = '*Préinscription* :\n\n' +
-                        '*Nom* : ' + nom +
-                        '\n*Prénom* : ' + prenom +
-                        '\n*Numéro WhatsApp* : ' + whatsapp +
-                        '\n\n*Session choisie* :\n' + session +
-                        '\n\n*Prix choisi* :\n' + price +
-                        '\n\n*Mode de paiement* :\n' + payment +
-                        '\n\n*Adresse du moyen de paiement* :\n' + paymentAddress;
-
-  // Créer le lien WhatsApp avec le message pré-rempli
-  var whatsappLink = 'https://wa.me/message/DJNWKADUL744A1?text=' + encodeURIComponent(whatsappMessage);
-
-  // Ouvrir une nouvelle fenêtre pour envoyer le message WhatsApp
-  window.open(whatsappLink);
-}
